ca30f9ae942830fa712abd4914260e537b0c9a53
TimmKroe <kroeger.timm@gmx.de>
2020-05-10 09:16:27 +0200

    added basic alerts / notification
    Signed-off-by: TimmKroe <kroeger.timm@gmx.de>

--- src/App.vue
+++ src/App.vue
@@ -3,53 +3,9 @@
 
 
         <div class="md:flex flex-col md:flex-row md:min-h-screen w-full">
-            <div class="flex flex-col w-full md:w-64 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0">
-                <div class="flex-shrink-0 px-8 py-4 flex flex-row items-center justify-between">
-                    <a class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
-                       href="#">HC Monitoring</a>
-                    <button class="rounded-lg md:hidden rounded-lg focus:outline-none focus:shadow-outline">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
-                            <path @click="toggleNav" clip-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                                  fill-rule="evenodd"
-                                  v-if="!open"></path>
-                            <path @click="toggleNav" clip-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                  fill-rule="evenodd"
-                                  v-if="open"></path>
-                        </svg>
-                    </button>
-                </div>
-                <nav :class="{'block': open, 'hidden': !open}"
-                     class="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">
-                    <div class="divide-y divide-gray-400">
-                        <div class="mb-2">
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                               href="/">Dashboard</a>
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                               >Servers</a>
-                            <div class="ml-6" v-for="(server, i) in 10" :key="i">
-                                <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                   :href="'/servers/d'">Servers</a>
-                            </div>
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                               href="/backups">Backups</a>
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                               href="/snapshots">Snapshots</a>
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                               href="/about">About</a>
-                        </div>
-
-
-                        <div class="">
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                               href="/org-management">ORG Management</a>
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                               href="/srv-management">SRV Management</a>
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                               href="/login">Login / Logout</a>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+            <!-- Sidebar -->
+            <SidebarNavigation/>
+            <!-- Content -->
             <div class="flex flex-col bg-gray-200 w-full">
                 <div class="absolute flex flex-row p-2 font-bold">
                     <div class="inline-block">
@@ -77,6 +33,13 @@
 
 
                 </div>
+
+                <div class="absolute bottom-0 right-0">
+                    <Alert class="mr-8 w-2/4" status="green" statustype="success" msg="sdadfaösjdfakjsdff"/>
+                    <Alert class="mr-8 w-2/4" status="green" statustype="success" msg="sdadfaösjdfakjsdff"/>
+                </div>
+
+
                 <div class="container mx-auto mt-20 md:p-10">
 
                     <router-view>
@@ -96,10 +59,12 @@
 <script>
     import { mapState, mapGetters } from 'vuex';
     import Footer from "@/components/Footer";
+    import Alert from "@/components/alerts/Alert";
+    import SidebarNavigation from "@/components/SidebarNavigation";
 
     export default {
         name: 'App',
-        components: {Footer},
+        components: {SidebarNavigation, Alert, Footer},
         computed: {
             ...mapState({
                 Authenticated: state => state.application.authenticated
@@ -115,6 +80,7 @@
         data() {
             return {
                 open: false,
+                notifications: []
             }
         },
         methods: {
@@ -126,5 +92,8 @@
 </script>
 
 <style>
-
+.center {
+    left: 50%;
+    transform: translate(-50%);
+}
 </style>
